7246bdbaf7a7322db9f614500c2c3c41f11f420d
Pranav Dhingra <pranav.dhingra@gmail.com>
2017-12-15 17:13:43 +0800

    Made basic UI components of Shop Info section

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,12 +1,70 @@
 import React, { Component } from 'react';
 
+import {List, ListItem} from 'material-ui/List';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import Divider from 'material-ui/Divider';
+import RaisedButton from 'material-ui/RaisedButton';
 
-class Location extends Component {
+import '../css/Location.css';
+
+class LocationItem extends Component {
+
+    // helps edit location details
+    locationItemOnClickHandler = (e) => {
+        // TODO
+    }
+
     render() {
         return (
-            <h1>
-                Shop Info
-            </h1>
+            <div>
+                <ListItem
+                    className='listItem'
+                    primaryText={
+                        <div className="list-flex-item, listUser">
+                          {this.props.location}
+                        </div>
+                    }
+                    secondaryText={
+                      <p>
+                        Opens: <span className="listTime"> {this.props.open} </span>
+                        <br/>
+                        Closes: <span className="listTime"> {this.props.close} </span>
+                      </p>
+                    }
+                    secondaryTextLines={2}
+                    onClick={this.locationItemOnClickHandler}
+                />
+                <Divider/>
+            </div>
+        );
+    }
+
+}
+
+class Location extends Component {
+    
+
+    // helps add location
+    buttonOnClickHandler = (e) => {
+        // TODO
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>
+                    Shop Info
+                </h1>
+                Click on shop to edit or remove
+                <br/><br/>
+                <MuiThemeProvider>
+                    <LocationItem location="Test location" open="an opening time" close="a closing time" />
+                    <LocationItem location="Test location 2" open="an opening time" close="a closing time" />
+                    <RaisedButton label="Add Shop" primary={true} fullWidth={false} style={{margin:12}}
+                        onClick={this.buttonOnClickHandler}
+                    />
+                </MuiThemeProvider>
+            </div>
         );
     }
 }
